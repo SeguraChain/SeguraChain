4c5add9f8660ea6ee4947b49a157759abb48312c
Xenophyte <96837446+SeguraChain@users.noreply.github.com>
2022-06-07 22:21:34 +0200

    Update
    > Update default keys.

--- SeguraChain-Lib/Blockchain/Setting/BlockchainSetting.cs
+++ SeguraChain-Lib/Blockchain/Setting/BlockchainSetting.cs
@@ -106,7 +106,7 @@ namespace SeguraChain_Lib.Blockchain.Setting
         public const int BlockDifficultyPrecision = 100_000; // The precision put on the difficulty factor calculated.
         public const int GenesisBlockHeight = 1; // The genesis block height.
         public static readonly BigInteger GenesisBlockAmount = 2_973_370 * CoinDecimal; // The genesis block amount reward has pre-mining.
-		public const string GenesisBlockFinalTransactionHash ="A6C5BF0A19D3C131F3E18DE9170BA05551189FE00C94EF02EC9D6D951D7682D0D49DA576E19E1CCE7CB0A0FCDA5736DAC1B870ABDB8104CBF9F6E7EB8B80D44D";
+		public const string GenesisBlockFinalTransactionHash ="5DAF3B2D553530B40F684E6D61F72C48FBB3D362859BEE4BFF4AC8499F7468555F06B30438F80D6D588B5563A3123994A6244D7129527C1A4F36C7DE60C4DE32";
         public const int GenesisBlockTransactionCount = 1; // The maximum of transaction inserted on the genesis block.
         public const int BlockAmountNetworkConfirmations = 2; // The minimum amount of network checks on blocks to do with peers, before to enable the task of confirmations on the block.
         public const int BlockAmountSlowNetworkConfirmations = 5; // This amount increment another amount of network checks, once this one is reach, the network counter increment and this one return back to 0.
