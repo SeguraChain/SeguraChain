2c8ea7c3513624adbaa53d862c5f9a253a27b3f8
Xenophyte <96837446+SeguraChain@users.noreply.github.com>
2023-09-12 11:17:34 +0200

    Update TaskManager.cs

--- SeguraChain-Lib/TaskManager/TaskManager.cs
+++ SeguraChain-Lib/TaskManager/TaskManager.cs
@@ -341,8 +341,20 @@ namespace SeguraChain_Lib.TaskManager
                         bool isLocked = false;
                         try
                         {
-                            isLocked = Monitor.TryEnter(_taskCollection);
+                            isLocked = Monitor.TryEnter(_taskCollection, 1000);
 
+                            while(!isLocked)
+                            {
+#if DEBUG
+                                Debug.WriteLine("Failed to lock taskCollection");
+#endif
+                                if (cancellationTask.IsCancellationRequested)
+                                    break;
+
+                                isLocked = Monitor.TryEnter(_taskCollection, 1000);
+
+                                Thread.Sleep(1);
+                            }
                             if (isLocked)
                             {
                                 _taskCollection.Add(new ClassTaskObject(action, cancellationTask, timestampEnd, socket));
