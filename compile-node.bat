29e773bb6256b24b64ec1e9601010ec902c07044
Xenophyte <96837446+SeguraChain@users.noreply.github.com>
2023-09-17 22:54:20 +0200

    Update compile-node.bat
    > Compilation for Net5 and Net6

--- compile-node.bat
+++ compile-node.bat
@@ -1,8 +1,20 @@
-dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Debug --framework net7.0 --output Output\Linux\x64\Debug\ --runtime linux-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
-dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Debug --framework net7.0 --output Output\Windows\x64\Debug\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Debug --framework net7.0 --output Output\Linux\x64\Debug\Net7\ --runtime linux-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Debug --framework net7.0 --output Output\Windows\x64\Debug\Net7\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
 
-dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Release --framework net7.0 --output Output\Linux\x64\Release\ --runtime linux-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
-dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Release --framework net7.0 --output Output\Windows\x64\Release\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Release --framework net7.0 --output Output\Linux\x64\Release\Net7\ --runtime linux-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Release --framework net7.0 --output Output\Windows\x64\Release\Net7\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Debug --framework net6.0 --output Output\Linux\x64\Debug\Net6\ --runtime linux-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Debug --framework net6.0 --output Output\Windows\x64\Debug\Net6\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Release --framework net6.0 --output Output\Linux\x64\Release\Net6\ --runtime linux-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Release --framework net6.0 --output Output\Windows\x64\Release\Net6\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Debug --framework net5.0 --output Output\Linux\x64\Debug\Net5\ --runtime linux-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Debug --framework net5.0 --output Output\Windows\x64\Debug\Net5\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Release --framework net5.0 --output Output\Linux\x64\Release\Net5\ --runtime linux-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Peer\SeguraChain-Peer.csproj --configuration Release --framework net5.0 --output Output\Windows\x64\Release\Net5\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
 
 
 pause
