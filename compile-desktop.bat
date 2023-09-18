e818f5ebb70e9c74c77e1d8d7dcf1da626b836ed
Xenophyte <96837446+SeguraChain@users.noreply.github.com>
2023-09-18 22:36:00 +0200

    Create compile-desktop.bat
    > Compile Desktop Wallet.

---
+++ compile-desktop.bat
@@ -0,0 +1,9 @@
+dotnet publish SeguraChain-Desktop-Wallet\SeguraChain-Desktop-Wallet.csproj --configuration Release --framework net7.0-windows --output Output\Windows\x64\Release\Desktop-Wallet\Net5\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Desktop-Wallet\SeguraChain-Desktop-Wallet.csproj --configuration Release --framework net6.0-windows --output Output\Windows\x64\Release\Desktop-Wallet\Net6\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Desktop-Wallet\SeguraChain-Desktop-Wallet.csproj --configuration Release --framework net5.0-windows --output Output\Windows\x64\Release\Desktop-Wallet\Net7\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+
+dotnet publish SeguraChain-Desktop-Wallet\SeguraChain-Desktop-Wallet.csproj --configuration Debug --framework net7.0-windows --output Output\Windows\x64\Debug\Desktop-Wallet\Net5\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Desktop-Wallet\SeguraChain-Desktop-Wallet.csproj --configuration Debug --framework net6.0-windows --output Output\Windows\x64\Debug\Desktop-Wallet\Net6\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+dotnet publish SeguraChain-Desktop-Wallet\SeguraChain-Desktop-Wallet.csproj --configuration Debug --framework net5.0-windows --output Output\Windows\x64\Debug\Desktop-Wallet\Net7\ --runtime win-x64 --verbosity quiet --self-contained true -p:PublishSingleFile=true
+
+pause
